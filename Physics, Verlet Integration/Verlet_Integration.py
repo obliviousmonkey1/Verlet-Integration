0126f044e59f945d42ac7248f6ed26b19c87bbc1
obliviousmonkey1 <70280680+obliviousmonkey1@users.noreply.github.com>
2021-09-15 17:56:52 +0100

    Update Verlet_Integration.py

--- Physics, Verlet Integration/Verlet_Integration.py
+++ Physics, Verlet Integration/Verlet_Integration.py
@@ -1,5 +1,6 @@
 import math
 import pygame
+import json
 
 pygame.init()
 
@@ -18,147 +19,34 @@ sticks = []
 bounce = 0.9
 gravity = 0.5
 friction = 0.999
+
+model = "Model2.json"
+
 angle = 0
 speed = 0.1
-
-pos = {
-    "x" : 100,
-    "y" : 100,
-    "oldx" : 95,
-    "oldy" : 95,
-    "pinned" : False
-    }
-pos2 = {
-    "x" : 200,
-    "y" : 100,
-    "oldx" : 200,
-    "oldy" : 100,
-    "pinned" : False
-    }
-pos3 = {
-    "x" : 200,
-    "y" : 200,
-    "oldx" : 200,
-    "oldy" : 100,
-    "pinned" : False
-    }
-pos4 = {
-    "x" : 100,
-    "y" : 200,
-    "oldx" : 100,
-    "oldy" : 200,
-    "pinned" : False
- 
-    }
-pos5 = {
-    "x" : 550,
-    "y" : 100,
-    "oldx" : 550,
-    "oldy" : 200,
-    "pinned" : True
- 
-    }
-pos6 = {
-    "x" : 400,
-    "y" : 100,
-    "oldx" : 400,
-    "oldy" : 100,
-    "pinned" : False
- 
-    }
-pos7 = {
-    "x" : 250,
-    "y" : 100,
-    "oldx" : 250,
-    "oldy" : 100,
-    "pinned" : False
- 
-    }
-
-
-
-points.append(pos)
-points.append(pos2)
-points.append(pos3)
-points.append(pos4)
-points.append(pos5)
-points.append(pos6)
-points.append(pos7)
-
 
 def distance(p0, p1):
     dx = p1["x"] - p0["x"]
     dy = p1["y"] - p0["y"]
     return math.sqrt(dx * dx + dy * dy)
 
-# p0 = 1 , p1 = 2
-sticks.append({
 
-    1: points[0],
-    2: points[1],
-    "length": distance(points[0], points[1]),
-    "hidden": False
-
-    })
-sticks.append({
-
-    1: points[1],
-    2: points[2],
-    "length": distance(points[1], points[2]),
-    "hidden": False
-
-    })
-sticks.append({
-
-    1: points[2],
-    2: points[3],
-    "length": distance(points[2], points[3]),
-    "hidden": False
-
-    })
-sticks.append({
-
-    1: points[3],
-    2: points[0],
-    "length": distance(points[3], points[0]),
-    "hidden": False
-
-    })
-sticks.append({
-
-    1: points[3],
-    2: points[1],
-    "length": distance(points[3], points[1]),
-    "hidden": True
-
-    })
-sticks.append({
-
-    1: points[4],
-    2: points[5],
-    "length": distance(points[4], points[5]),
-    "hidden": False
-
-    })
-sticks.append({
-
-    1: points[5],
-    2: points[6],
-    "length": distance(points[5], points[6]),
-    "hidden": False
-
-    })
-sticks.append({
-
-    1: points[6],
-    2: points[0],
-    "length": distance(points[6], points[0]),
-    "hidden": False
-
-    })
+def loadModel(model_data):
+    for i in range(0, len(model_data["points"])):
+        points.append(model_data["points"][i])
+ 
+    for i in range(0, len(model_data["sticks"])):
+        s = model_data["sticks"][i]
+        s["1"] = points[s["1"]]
+        s["2"] = points[s["2"]]
+        s["length"] = distance(s["1"], s["2"])
+        sticks.append(s)
 
 
+with open(f'D:\Python\Physics, Verlet Integration\Models\{model}' , "r") as f:
+    model_data = json.load(f)
 
+loadModel(model_data)
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Verlet Integration')
@@ -174,7 +62,7 @@ def update():
         updateSticks()
         constrainPoints()
     screen.fill(WHITE)
-    #renderPoints()
+    renderPoints()
     renderSticks()
   
     pygame.display.update(update())
@@ -184,7 +72,7 @@ def update():
 def updatePoints():
     for i in range(0, len(points)):
         p = points[i]
-        if p["pinned"] == False:
+        if p["pinned"] == 0:
             vx = (p["x"] - p["oldx"]) * friction
             vy = (p["y"] - p["oldy"]) * friction
 
@@ -203,7 +91,7 @@ def constrainPoints():
         width of the screen and then we basically flip the oldx value so that we get a
         change of volocity onto the screen.
         """
-        if p["pinned"] == False:
+        if p["pinned"] == 0:
             vx = (p["x"] - p["oldx"]) * friction
             vy = (p["y"] - p["oldy"]) * friction
 
@@ -224,23 +112,26 @@ def constrainPoints():
                 p["oldy"] = p["y"] + vy * bounce
 
 def updateSticks():
-    for i in range(0, len(sticks)):
-        s = sticks[i]
-        dx = s[2]["x"]-  s[1]["x"]
-        dy = s[2]["y"] - s[1]["y"]
-        distance = math.sqrt(dx * dx + dy * dy)
-        difference = s["length"] - distance
-        # distance each point will have to move to put it in the right spot
-        percent = difference / distance / 2
-        offsetX = dx*percent
-        offsetY = dy*percent
+    if len(sticks) == 0:
+        pass
+    elif len(sticks) > 0 :
+        for i in range(0, len(sticks)):
+            s = sticks[i]
+            dx = s["2"]["x"]-  s["1"]["x"]
+            dy = s["2"]["y"] - s["1"]["y"]
+            distance = math.sqrt(dx * dx + dy * dy)
+            difference = s["length"] - distance
+            # distance each point will have to move to put it in the right spot
+            percent = difference / distance / 2
+            offsetX = dx*percent
+            offsetY = dy*percent
 
-        if s[1]["pinned"] == False:
-            s[1]["x"] -= offsetX
-            s[1]["y"] -= offsetY
-        if s[2]["pinned"] == False:
-            s[2]["x"] += offsetX
-            s[2]["y"] += offsetY
+            if s["1"]["pinned"] == False:
+                s["1"]["x"] -= offsetX
+                s["1"]["y"] -= offsetY
+            if s["2"]["pinned"] == False:
+                s["2"]["x"] += offsetX
+                s["2"]["y"] += offsetY
 
     
 
@@ -265,19 +156,22 @@ def renderPoints():
         pygame.display.flip()
 
 def renderSticks():
-    for i in range(0, len(sticks)):
-        s = sticks[i]
+    print(len(sticks))
+    if len(sticks) == 0:
+        pass
+    elif len(sticks) > 0 :
+        for i in range(0, len(sticks)):
+            s = sticks[i]
 
-        if s["hidden"] != True:
-            
-            a = s[1]["x"] , s[1]["y"]
-            b = s[2]["x"] , s[2]["y"]
-            pygame.draw.line(screen, BLACK,a, b, 2)
+            if s["hidden"] == 0:
+                
+                a = s["1"]["x"] , s["1"]["y"]
+                b = s["2"]["x"] , s["2"]["y"]
+                pygame.draw.line(screen, BLACK,a, b, 2)
 
-        clock.tick(60)
-        pygame.display.flip()
+            clock.tick(60)
+            pygame.display.flip()
         
 
 update()
-    
 
